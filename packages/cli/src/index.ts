8fa8a89941439a8909f2cbec07a0547707fd10aa
Feavy <quentin071100@live.fr>
2025-02-17 22:58:48 +0100

    Setup nx monorepo

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,8 +2,19 @@ import WorkspaceConfigRenderer from './config/WorkspaceConfigRenderer.js';
 import { fromError } from 'zod-validation-error';
 import KubernetesWorkspace from './kubernetes/KubernetesWorkspace.js';
 import * as yaml from 'yaml'
+import { lib, lib2 } from '@workspace/lib'
+
+import * as path from 'path';
+
+console.log(lib(), lib2());
 
 try {
+    if(import.meta.env?.DEV) {
+        console.log('Running in development mode');
+    } else {
+        console.log('Running in production mode');
+    }
+    console.log(import.meta.env);
     const configRenderer = new WorkspaceConfigRenderer('workspace.yml');
     const workspaceConfig = configRenderer.render();
 
---
+++ packages/lib/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/lib.js';
+export * from './lib/lib2.js';
